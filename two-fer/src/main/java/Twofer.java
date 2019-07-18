2f143b24b453ae16af18e61d4d2dd754a940963f
Michal Stoma <michal.stoma@tivo.com>
2019-07-18 10:11:46 +0200

    Two Fer: second approach; use Optional for better readability.

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,9 @@
+import java.util.Optional;
+
 class Twofer {
+	private static final String DEFAULT_NAME = "you";
+
     String twofer(String name) {
-    	return String.format("One for %s, one for me.", name == null ? "you" : name);
+    	return String.format("One for %s, one for me.", Optional.ofNullable(name).orElse(DEFAULT_NAME));
     }
 }
