ef2eee3c90cefa9f81477fc63c97fe7b40c98235
Michal Stoma <m.stoma@gmail.com>
2019-07-26 19:27:01 +0200

    Hamming: second iteration.

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -2,54 +2,37 @@ class Hamming {
 
 	private static String leftStrand;
 	private static String rightStrand;
-	private static int strandLength;
-	private static int hammingDistance;
 
     Hamming(String leftStrand, String rightStrand) {
         
         this.leftStrand = leftStrand;
         this.rightStrand = rightStrand;
+
+        String exceptionMessage;
         
-        this.setStrandsLength();
-        this.setHammingDistance();
-    }
+        if (this.leftStrand.length() != this.rightStrand.length()) {
+        	exceptionMessage = "leftStrand and rightStrand must be of equal length.";
 
-    private void setStrandsLength() {
-    	int left = this.leftStrand.length();
-    	int right = this.rightStrand.length();
-    	String exceptionMessage;
-
-    	if (left != right) {
-    		exceptionMessage = "leftStrand and rightStrand must be of equal length.";
-
-        	if (left == 0) {
+        	if (this.leftStrand.length() == 0) {
         		exceptionMessage = "left strand must not be empty.";
-        	} else if (right == 0) {
+        	} else if (this.rightStrand.length() == 0) {
         		exceptionMessage = "right strand must not be empty.";
         	}
 
         	throw new IllegalArgumentException(exceptionMessage);
         }
-
-        this.strandLength = left;
-    }
-
-    private void setHammingDistance() {
-    	this.hammingDistance = 0;
-    	
-    	for (int i = 0; i < this.strandLength; i++) {
-    		char leftChar = this.leftStrand.charAt(i);
-    		char rightChar = this.rightStrand.charAt(i);
-
-    		if (leftChar != rightChar) {
-    			this.hammingDistance++;
-    		}
-    	}
-
     }
 
     int getHammingDistance() {
-    	return this.hammingDistance;
+    	int hammingDistance = 0;
+
+    	for (int i = 0; i < this.leftStrand.length(); i++) {
+    		if (this.leftStrand.charAt(i) != this.rightStrand.charAt(i)) {
+    			hammingDistance++;
+    		}
+    	}
+
+    	return hammingDistance;
     }
 
 }
