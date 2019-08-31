61b957f515d8f619fb9145d669d99d82c2812f68
Michal Stoma <m.stoma@gmail.com>
2019-08-31 22:17:11 +0200

    Scrabble Score: second iteration

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,30 +1,63 @@
-import java.util.Map;
-import java.util.HashMap;
-
 class Scrabble {
-
-	private final static Map<Integer, Integer> SCORES = new HashMap<Integer, Integer>();
-
-	static {
-		"DG".chars().forEach(c -> SCORES.put(c, 2));
-		"BCMP".chars().forEach(c -> SCORES.put(c, 3));
-		"FHVWY".chars().forEach(c -> SCORES.put(c, 4));
-		"K".chars().forEach(c -> SCORES.put(c, 5));
-		"JX".chars().forEach(c -> SCORES.put(c, 8));
-		"QZ".chars().forEach(c -> SCORES.put(c, 10));
-	}
-
-	private static String word;
+    private final int score;
 
     Scrabble(String word) {
-        this.word = word;
+        this.score = this.calculateScore(word.toUpperCase());
+    }
+
+    private int calculateScore(String word) {
+        int sum = 0;
+
+        for (int i=0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            sum += this.getSingleScore(letter);
+        }
+
+        return sum;
+    }
+
+    private int getSingleScore(char letter){
+        switch (letter) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+        }
     }
 
     int getScore() {
-        return this.word.toUpperCase()
-	        			.chars()
-	        			.map(c -> SCORES.getOrDefault(c, 1))
-	        			.sum();
+        return this.score;
     }
 
 }
